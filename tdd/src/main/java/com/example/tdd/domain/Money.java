beb581f50b75d8a8a4b07e7af4b9486d78888404
JYH <verin4494@gmail.com>
2022-04-27 01:25:03 +0900

    객체 생성

---
+++ tdd/src/main/java/com/example/tdd/domain/Money.java
@@ -0,0 +1,21 @@
+package com.example.tdd.domain;
+
+public abstract class Money {
+
+    protected int amount;
+
+    public static Money dollar(int amount) {
+        return new Dollar(amount);
+    }
+
+    public static Money franc(int amount) {
+        return new Franc(amount);
+    }
+
+    public boolean equals(Object object) {
+        Money money = (Money) object;
+        return amount == money.amount && getClass().equals(money.getClass());
+    }
+
+    public abstract Money times(int multiplier);
+}
--- tdd/src/test/java/com/example/tdd/TddApplicationTests.java
+++ tdd/src/test/java/com/example/tdd/TddApplicationTests.java
@@ -1,22 +1,42 @@
 package com.example.tdd;
 
 import com.example.tdd.domain.Dollar;
+import com.example.tdd.domain.Franc;
+import com.example.tdd.domain.Money;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.Assertions.*;
 
-@SpringBootTest
+//@SpringBootTest
 class TddApplicationTests {
 
 	@Test
+	@DisplayName("testMultiplication")
 	public void testMultiplication() {
-		Dollar five = new Dollar(5);
-		Dollar product = five.times(2);
-		assertThat(product.amount).isEqualTo(10);
-		product = five.times(3);
-		assertThat(product.amount).isEqualTo(15);
+		Money five = Money.dollar(5);
+		assertThat(five.times(2)).isEqualTo(Money.dollar(10));
+		assertThat(five.times(3)).isEqualTo(Money.dollar(15));
+	}
+
+	@Test
+	@DisplayName("testFrancMultiplication")
+	public void testFrancMultiplication() {
+		Franc five = new Franc(5);
+		assertThat(five.times(2)).isEqualTo(new Franc(10));
+		assertThat(five.times(3)).isEqualTo(new Franc(15));
+	}
+
+	@Test
+	@DisplayName("testEquality")
+	public void testEquality() {
+		assertThat(Money.dollar(5).equals(Money.dollar(5))).isTrue();
+		assertThat(Money.dollar(5).equals(Money.dollar(6))).isFalse();
+		assertThat(new Franc(5).equals(new Franc(5))).isTrue();
+		assertThat(new Franc(5).equals(new Franc(6))).isFalse();
+		assertThat(new Franc(5).equals(Money.dollar(5))).isFalse();
 	}
 
 }
